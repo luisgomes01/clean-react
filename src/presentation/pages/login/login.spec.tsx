8b833d358d747bb18da786db1b6d0f8da5de84f7
Luís Gomes <luis252001@hotmail.com>
2023-05-17 10:32:49 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,5 +7,7 @@ describe('Login Component', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
