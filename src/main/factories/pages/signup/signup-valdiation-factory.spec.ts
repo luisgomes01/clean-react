69902294cbc86ce05143f3faf870bcf781f922db
Luís Gomes <luis252001@hotmail.com>
2023-11-08 14:20:23 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-valdiation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { ValidationComposite } from '@/validation/validators'
+import { makeSignupValidation } from './signup-validation-factory'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+describe('makeSignupValidation', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignupValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
