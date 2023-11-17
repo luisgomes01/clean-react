fcd40d3a968bfb3d549f541beba2909ac4f8b430
Luís Gomes <luis252001@hotmail.com>
2023-11-17 09:38:37 -0300

    test: ensure Login presents  error message if invalid credentials are provided

--- src/main/test/cypress/e2e/login.cy.ts
+++ src/main/test/cypress/e2e/login.cy.ts
@@ -1,5 +1,7 @@
 import { faker } from '@faker-js/faker'
 
+const baseUrl: string = Cypress.config().baseUrl
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -44,5 +46,29 @@ describe('Login', () => {
       .should('contain.text', '🟢')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus()
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').focus()
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('contain.text', 'Unexpected Error was thrown. Try again soon.')
+    cy.url().should('equal', `${baseUrl}/login`)
+  })
+
+  it('Should save accessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').focus()
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').focus()
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('contain.text', 'Unexpected Error was thrown. Try again soon.')
+    cy.url().should('equal', `${baseUrl}/login`)
   })
 })
